4da0c9f0452f61daa15de5d851e18620b06c52c5
brad fox <bradfox2@gmail.com>
2019-06-08 15:56:20 -0700

    File conversion via unoconv server.

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from wip import *
--- wip/convert.py
+++ wip/convert.py
@@ -1,40 +1,127 @@
+'''Convert file via request to Unoconv.'''
 import os
-import re
-from pathlib import Path
-
-import win32com.client as win32
-from win32com.client import constants
 import platform
+import re
+from pathlib import Path, PurePath
+import itertools
+
 import click
+import requests
+import warnings
 
-def check_os():
-    return "win" if platform.system() == 'Windows' else 'nix'
+def build_conversion_url(conversion_url, file_type):
+    return str(conversion_url + 'unoconv/' + file_type)
 
-@click.command()
-@click.option('--path', help='Path to file for conversion.')
-# Create list of paths to .doc files
-def save_as_docx(path):
-    if check_os() == 'Windows':
-        #path = Path(path).glob('**\\7787538.doc')
+class Unoconv(object):
+    
+    """ Representation of a Unoconv server that will be used to convert documents.
+    
+    Arguments:
+        url {str} -- Base URL of the Unoconv server.
+    
+    Properties:
+        url {str} -- Base URL of Unoconv server, if it exists.
+        formats {dict} -- Possible conversion formats.
+        update {num} -- Current uptime.
+    """
+    
+    def __init__(self, url):
+        if requests.get(url).text is not None:
+            self.url = url
+            self.doc_types = requests.get(url + '/unoconv/formats').json()['document'] 
+            self.extensions = {fmt['extension'] for fmt in self.doc_types}
+            self.formats = {fmt['format'] for fmt in self.doc_types}
+            self.fmt2ext = {f['format']:f['extension'] for f in self.doc_types}
+        else:
+            ValueError('Initial request to {} failed.'.format(url))
+    
+    @property
+    def uptime(self):
+        r = requests.get(self.url + '/healthz')
+        return r.json()['uptime']
+    
+    @uptime.setter
+    def uptime(self):
+        warnings.warn('No setting uptime.')
 
-        # Opening MS Word
-        word = win32.gencache.EnsureDispatch('Word.Application')
-        doc = word.Documents.Open(path)
-        doc.Activate ()
-
-        # Rename path with .docx
-        new_file_abs = os.path.abspath(path)
-        new_file_abs = re.sub(r'\.\w+$', '.docx', new_file_abs)
-
-        # Save and Close
-        word.ActiveDocument.SaveAs(
-            new_file_abs, FileFormat=constants.wdFormatXMLDocument
-        )
-        doc.Close(False)
-
-        return new_file_abs
-    else:
-        pass
+    def build_conversion_url(self, file_format):
+        """Constructs conversions URL based on format like /unoconv/{format-to-convert-to} per https://github.com/zrrrzzt/docker-unoconv-webservice
         
+        Arguments:
+            file_format {str} -- Converted file format type, not necessarily the extension.
+        
+        Returns:
+            str -- URL that will convert to this file type.
+        """
+        return self.url + '/unoconv/' + self.fmt2ext[file_format]
+
+def request_to_file(r, file_name, save_to_dir):
+    """Save a request object contents to file.
+    
+    Arguments:
+        r {Request} -- request object
+        file_name {str} -- file name
+    """
+    path = PurePath().joinpath(save_to_dir, file_name)
+    with open(path, 'wb') as f:
+            f.write(r.content)
+    return True
+
+def convert_file(unoconv, path, file_format, save_dir):
+    """Convert file at path to a new file type by using Unoconv object.
+    
+    Arguments:
+        conversion_url {Unoconv} -- Unoconv object that points at Unoconv server used for file conversion.
+        path {str} -- Path of file that will be converted or directory of files
+        file_format {str} -- File will be converted to this format.
+
+    Returns:
+        boolean - True if successful, None if not. 
+    """
+    
+    #//TODO: Allow path to already be a Path object.
+
+    path = Path(path)
+    if path.exists:
+        file = {'file': open(path, 'rb')}
+    else:
+        return ValueError('{} does not exist.'.format(path))
+    if file_format not in unoconv.formats:
+        return ValueError('Cannot convert to {} in this instance of Unoconv'.format(file_format))
+
+    url = unoconv.build_conversion_url(file_format)
+    r = requests.post(url, files=file)
+    if r:
+        file_name = path.stem +'.' + unoconv.fmt2ext[file_format]
+        return True if request_to_file(r, file_name, save_dir) else None
+    else:
+        warnings.warn('Status Code {} from {}'.format(r.status_code, url))
+        return None
+
+def convert(unoconv, path, ext, file_format, save_dir):
+    """Convert file at path or files in directory at path to file format.
+    
+    Arguments:
+        unoconv {Unoconv} -- Unoconv object
+        path {str} -- Directory path where files reside.
+        file_format {str} -- Convert to this format.
+    """
+    file_path = Path(path)
+    save_dir = Path(path)
+    save_dir.mkdir(exist_ok=True)
+    
+    if file_path.is_dir():
+        files_0, files_1 = itertools.tee(file_path.glob('*'+'.'+ext)) #clone the generator so we can see the count of files
+        len_files = sum(1 for _ in files_0) 
+        print('{} files for conversion.'.format(len_files))
+        for file_ in enumerate(files_1):
+            print(file_)
+            #//TODO:
+            #convert_file(unoconv, str(file_), file_format, save_dir)
+    else:
+        convert_file(unoconv, file_path, file_format, save_dir)
+
 if __name__ == "__main__":
-    save_as_docx()
+    u = Unoconv('http://s1:3000')
+    #convert_file(u, 'docs/7787538.DOC', 'docx', 'converted')
+    convert(u, 'docs', 'docx', 'converted', 'doc')
