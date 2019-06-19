18a0879b5df6051090815b7a808a67105e5a9727
Fox <Z08273@apsc.com>
2019-06-19 08:27:12 -0700

    tsne working

--- wordparsing/metrics/metrics.py
+++ wordparsing/metrics/metrics.py
@@ -1,3 +1,5 @@
+''' produce metrics and other visualizations from the storage database'''
+
 import json
 from hashlib import sha256
 from pathlib import Path
@@ -8,6 +10,9 @@ import numpy as np
 from bert_serving.client import BertClient
 from matplotlib import pyplot as plt
 from sklearn.manifold import TSNE
+# Cos Sim
+from sklearn.metrics.pairwise import cosine_similarity as cossim
+from sqlalchemy.orm import Query
 
 from wordparsing.convert import Unoconv, convert
 # Embed
@@ -24,29 +29,52 @@ from wordparsing.storage.data_classes import (Embedding, Model, Session,
 from wordparsing.utils import cos_sim, heirarchical_dict_to_flat_list
 
 # TSNE
-sess = Session(engine)
-emb = sess.query(Embedding).all()[:250]
-doc_array = np.array([e.vector for e in emb])
-target_names = [e.uuid for e in emb]
-X_embedded = TSNE(n_components=2).fit_transform(doc_array)
-target_ids = range(len(target_names))
-plt.figure(figsize=(6, 5))
-colors = 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'w', 'orange', 'purple'
-for i, label in zip(target_ids, target_names):
-    plt.scatter(X_embedded[i,0], X_embedded[i,1])# label=label)
-plt.legend()
-plt.show()
+def generate_tsne(query):
+    """Generate a TSNE Plot of Embedding vectors.
 
-# Cos Sim
-from sklearn.metrics.pairwise import cosine_similarity as cossim
-cs = cossim(doc_array)
+    Arguments:
+        query {sqlalchemy.orm.Query} -- SQLAlchemy query object that will return embedding objects.
+    """
 
-l1 = cs[15]
-l1,l2 = zip(*sorted(zip(l1,target_names)))  #TODO: not working
-embobj = sess.query(Embedding).filter(Embedding.uuid.in_((l2[:2]))).all()
-epk = [e.textpart_pk for e in embobj]
-[e.textpart_pk for e in sess.query(TextPart).filter(TextPart.textpart_pk.in_(epk)).all()]
+    sess = Session(engine)
+    emb = query.all()
+    doc_array = np.array([e.vector for e in emb])
+    target_names = [e.uuid for e in emb]
+    X_embedded = TSNE(n_components=2).fit_transform(doc_array)
+    target_ids = range(len(target_names))
+    plt.figure(figsize=(6, 5))
+    colors = 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'w', 'orange', 'purple'
+    for i, label in zip(target_ids, target_names):
+        plt.scatter(X_embedded[i,0], X_embedded[i,1])#, label=label)
+    plt.legend()
+    plt.show()
+
+def cos_sim(query):
+     """Generate pairwise cosine simliarities of Embedding vectors.
+
+    Arguments:
+        query {sqlalchemy.orm.Query} -- SQLAlchemy query object that will return embedding objects.
+    """
+    raise NotImplementedError
+    #TODO: fix cosine similarity
+    
+    #emb = query
+    #doc_array = np.array([e.vector for e in emb])
+    #target_names = [e.uuid for e in emb]
+    #cs = cossim(doc_array)
+
+    #l1 = cs[15]
+    #l1,l2 = zip(*sorted(zip(l1,target_names)))  #TODO: not working
+    #list1, list2 = map(l1, zip(*zip(l1,target_names)))
+    #embobj = sess.query(Embedding).filter(Embedding.uuid.in_((l2[:2]))).all()
+    #epk = [e.textpart_pk for e in embobj]
+    #[e.textpart_pk for e in sess.query(TextPart).filter(TextPart.textpart_pk.in_(epk)).all()]
 
 
-
+if __name__ == "__main__":
+    # Example TSNE
+    sess = Session(engine)
+    query = sess.query(Embedding)
+    TSNE(query)
+    sess.close()
 
